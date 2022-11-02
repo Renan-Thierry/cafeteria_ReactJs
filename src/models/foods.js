b810d8d8144d08d36e439e7b077d00b5b5120cf5
Renan-Thierry <90737490+Renan-Thierry@users.noreply.github.com>
2022-11-02 03:35:57 -0300

    Add files via upload

---
+++ src/models/foods.js
@@ -0,0 +1,13 @@
+const foods = [
+  {
+    name: "Hamburguer",
+    image: "img/hamburguer.jpg"
+  },
+  {
+    id: 2,
+    name: 'Sanduíche',
+    image: 'img/Sanduiche.jpg',
+  },
+];
+
+export default foods;
